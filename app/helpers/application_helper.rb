7e36f54dd0af7baf6b83d7ee5b32081f3b2ee213
EC2 Default User <ec2-user@ip-172-31-37-48.us-east-2.compute.internal>
2019-11-18 03:36:50 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを返す
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty? # enpty?はその中身が空ならばtrueを返す
+      base_title　# trueの場合は上で定義したbase_titleを返す
+    else
+      page_name + " | " + base_title # falseの場合（page_nameに何か入っている場合）の処理
+    end
+  end
+  
 end
