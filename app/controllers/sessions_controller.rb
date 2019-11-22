48eb21a306768fbc9d599661dc1401d9c437c947
EC2 Default User <ec2-user@ip-172-31-37-48.us-east-2.compute.internal>
2019-11-22 22:27:00 +0000

    ログイン失敗時の処理

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase) # ログインフォームから受け取ったemailの値を使ってユーザーオブジェクトを検索しuserに代入。
+    if user && user.authenticate(params[:session][:password]) # has_secure_passwordの機能であるauthenticateメソッドを利用して認証の実行。
+      # ログイン後にユーザー情報ページにリダイレクトする。
+    else
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
   end
 end
