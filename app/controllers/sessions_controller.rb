daa149c7140062e5b47d1d6e55393e136bdc207a
EC2 Default User <ec2-user@ip-172-31-37-48.us-east-2.compute.internal>
2019-11-23 16:18:04 +0000

    Finish login-function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,18 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) # ログインフォームから受け取ったemailの値を使ってユーザーオブジェクトを検索しuserに代入。
     if user && user.authenticate(params[:session][:password]) # has_secure_passwordの機能であるauthenticateメソッドを利用して認証の実行。
       # ログイン後にユーザー情報ページにリダイレクトする。
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
+  
+    def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user # 保存成功後、ログインする。
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
