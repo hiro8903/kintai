6f132d0f72753fdd7efd9a7304006106f021aab7
EC2 Default User <ec2-user@ip-172-31-37-48.us-east-2.compute.internal>
2019-12-05 15:33:50 +0000

    ユーザー一覧ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
   end
-  
+
   def new
     @user = User.new
   end
-  
 
   def create
     @user = User.new(user_params)
@@ -21,10 +24,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
@@ -34,20 +37,20 @@ class UsersController < ApplicationController
     end
   end
 
-  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
     # beforeフィルター
-    
-    # paramsハッシュからユーザーを取得します。	# ログイン済みのユーザーか確認します。
-    def set_user	
-      @user = User.find(params[:id])	
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
     end
 
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
         store_location
@@ -56,7 +59,6 @@ class UsersController < ApplicationController
       end
     end
 
-    
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
