2afeaf052b7445ca2e2fc329d5902ec5e22e7881
Hiroki Araki <hiro8903@gmail.com>
2021-03-12 22:05:37 +0900

    Accordion function on the edit button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info, :show_one_week]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :show_one_week]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info, :index]
+  before_action :correct_user, only: [:edit]
+  before_action :admin_user, only: [:update, :destroy, :edit_basic_info, :update_basic_info, :index]
   before_action :admin_or_correct_user, only: [:edit]
   before_action :admin_or_correct_user_or_requesting_user, only:[:show]
   before_action :set_one_month, only: [:show, :show_one_week]
@@ -61,10 +61,11 @@ class UsersController < ApplicationController
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
-      redirect_to @user
+      redirect_to users_url
     else
-      render :edit      
+      render :index      
     end
+    
   end
 
   def destroy
